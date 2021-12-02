64f40f18d61995ff5044fc600bcf0c843e56bc81
Subhadeep <subhadeep0326@gmail.com>
2021-12-02 13:13:46 +0530

    [Subhadeep] add calculate daily wage method

--- EmployeeWageComputation.py
+++ EmployeeWageComputation.py
@@ -10,14 +10,40 @@ import random
 Is_Absent = 0
 Is_Present = 1
 
-attendance = random.randint(0,1)
 
-match attendance:
-    case 0: 
-        Is_Absent
-        print("Employee is absent")
+def check_attendance():
+    '''
+    Description: Checks the employee attendace.
+    '''
+    attendance = random.randint(0,1)
+    match attendance:
+        case 0: 
+            print("Employee is absent")
+            return Is_Absent
+        
+        case 1: 
+            print("Employee is present")
+            return Is_Present
+
+def calc_daily_wage():
+    '''
+    Description: This function calculates the daily employee wage.
+    Returns: The daily wage.
+    '''
+    wage_per_hour = 20
+    full_day_hour = 8
+
+    if (check_attendance() == Is_Present):
+        daily_wage = wage_per_hour * full_day_hour
     
-    case 1: 
-        Is_Present
-        print("Employee is present")
+    else:
+        daily_wage = 0
+    
+    return daily_wage
 
+
+
+if __name__ == '__main__':
+    print(calc_daily_wage())
+
+    
